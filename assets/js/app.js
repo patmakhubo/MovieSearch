fb43097e15a97a22fcd8be0c4e133ad1e820c1ef
Thabang Pat Makhubo <45233350+patmakhubo@users.noreply.github.com>
2020-09-01 08:08:33 +0200

    Updated registration form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 //TMDB Initial Values
-const API_KEY = '44bd07fbb81022e81679c06c7a5705c9';
+const API_KEY = '5ec279387e9aa9488ef4d00b22acc451';
 //getCookie('tmdbapikey');
 // Endpoint
-const endpoint = 'http://api.themoviedb.org/3/search/movie?api_key=44bd07fbb81022e81679c06c7a5705c9';
+const endpoint = 'http://api.themoviedb.org/3/search/movie?api_key='+API_KEY;
 // getCookie('tmdbendpointurl');
 const movieURL = 'https://image.tmdb.org/t/p/w500';
 
